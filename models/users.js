5f10764983abcfe1a6e55b0049285db6aafaf470
mykeldee <kdankyira.development@gmail.com>
2022-08-21 02:45:50 +0000

    Update models and routes

--- models/users.js
+++ models/users.js
@@ -3,12 +3,16 @@ const { Schema } = mongoose;
 mongoose.Promise = global.Promise;
 
 const userSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, default: null },
-    password: { type: String, default: null },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
     dateJoined: { type: Date, default: Date.now() },
-    userLevel: { type: String, default: null }
+    userLevel: { type: String, enum:["user", "staff", "manager", "admin"], default: "not assigned" },
+    isStaff: { type: Bolean, default: 0 },
+    isManager: { type: Bolean, default: 0 },
+    isAdmin: { type: Bolean, default: 0 }
 },
+{ timestamps: true },
 { collection : 'users' });
 
 module.exports = mongoose.models.users || mongoose.model("users", userSchema);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { check } = require('express-validator')
 
 const router = express.Router();
 const controller = require('../controllers/userController');
 
-router.post('/auth/register', controller.logIn)
+router.post('/auth/login', [check("email", "Please enter a valid email").isEmail, check("password", "A valid password is required").exists()], controller.logIn)
 
-router.post('/auth/signUp', controller.register)
+router.post('/auth/register', controller.register)
 
 module.exports = router;
 
